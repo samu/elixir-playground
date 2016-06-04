67f35482aaf0500cd6d8dfb9b347d15bd84196f1
Samuel Mueller <mueller.samu@gmail.com>
2016-06-04 18:20:46 +0200

    proper module syntax

--- lib/elixir_playground/ping.ex
+++ lib/elixir_playground/ping.ex
@@ -1,10 +1,8 @@
-defmodule ElixirPlayground do
-  defmodule Ping do
-    def start do
-      receive do
-        {:pong, receiver} -> send receiver, :ping
-      end
-      start
+defmodule ElixirPlayground.Ping do
+  def start do
+    receive do
+      {:pong, receiver} -> send receiver, :ping
     end
+    start
   end
 end
