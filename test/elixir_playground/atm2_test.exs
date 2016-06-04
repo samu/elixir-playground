95ac164ed2a0870867337ca7ef4087d01b9f302e
Samuel Mueller <mueller.samu@gmail.com>
2016-06-04 19:13:21 +0200

    use setup in test

--- test/elixir_playground/atm2_test.exs
+++ test/elixir_playground/atm2_test.exs
@@ -2,23 +2,25 @@ defmodule Atm2Test do
   use ExUnit.Case
   alias ElixirPlayground.Atm2
 
-  test "it allows get the balance" do
+  setup do
     {:ok, pid} = Atm2.start 10
+    {:ok, %{atm: pid}}
+  end
+
+  test "it allows get the balance", %{atm: pid} do
     balance = Atm2.get_balance pid
     assert balance == 10
   end
 
-  test "it allows deposit" do
-    {:ok, pid} = Atm2.start 0
+  test "it allows deposit", %{atm: pid} do
     Atm2.deposit pid, 10
     balance = Atm2.get_balance pid
-    assert balance == 10
+    assert balance == 20
   end
 
-  test "it allows withdraw" do
-    {:ok, pid} = Atm2.start 100
-    Atm2.withdraw pid, 10
+  test "it allows withdraw", %{atm: pid} do
+    Atm2.withdraw pid, 5
     balance = Atm2.get_balance pid
-    assert balance == 90
+    assert balance == 5
   end
 end
